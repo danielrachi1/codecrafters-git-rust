dea9235bb07bdbd5ad2ec0834b35d538385d32a9
danielrachi <hi@danielrachi.com>
2026-01-21 16:17:03 -0500

    Add CLI skeleton

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
 pub struct Cli {
@@ -9,4 +9,29 @@ pub struct Cli {
 #[derive(Subcommand)]
 pub enum Commands {
     Init,
+    CatFile(CatFileArgs),
+}
+
+#[derive(Args)]
+pub struct CatFileArgs {
+    #[command(flatten)]
+    pub mode: CatFileMode,
+
+    pub hash: String,
+}
+
+#[derive(Args)]
+#[group(required = true, multiple = false)]
+pub struct CatFileMode {
+    #[arg(short = 't')]
+    pub show_type: bool,
+
+    #[arg(short = 's')]
+    pub size: bool,
+
+    #[arg(short = 'p')]
+    pub print: bool,
+
+    #[arg(short = 'e')]
+    pub exists: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,21 @@ fn main() {
             fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
             println!("Initialized git directory")
         }
+        Commands::CatFile(args) => {
+            let mode = &args.mode;
+            let hash = &args.hash;
+
+            if mode.show_type {
+                println!("-t: {hash}");
+            } else if mode.size {
+                println!("-s: {hash}");
+            } else if mode.print {
+                println!("-p: {hash}");
+            } else if mode.exists {
+                println!("-e: {hash}");
+            } else {
+                unreachable!() // clap guarantees exactly one is true
+            }
+        }
     }
 }
